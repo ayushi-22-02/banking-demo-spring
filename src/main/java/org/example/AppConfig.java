8a8f1f4ea14f75255e18d28c39dd95739e1dbff2
Ayushi <ayushi.khandelwal@credable.in>
2022-02-22 11:13:31 +0530

    addind repository and component scan

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -6,21 +6,24 @@ import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan({"org.example"})
 public class AppConfig {
 
-    @Bean("customerService")
-    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
-    public CustomerService getCustomerService() {
-        CustomerService service = new DefaultCustomerService(getCustomerRepositoryDB());
-        return service;
-    }
-
-    @Bean("customerRepository")
-    public CustomerRepository getCustomerRepositoryDB() {
-        return new CustomerRepositoryDB();
-    }
+//    @Bean("customerService")
+//    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+//    public CustomerService getCustomerService() {
+//        CustomerService service = new DefaultCustomerService(getCustomerRepositoryDB());
+//        return service;
+//    }
+//
+//    @Bean("customerRepository")
+//    public CustomerRepository getCustomerRepositoryDB() {
+//        return new CustomerRepositoryDB();
+//    }
 }
