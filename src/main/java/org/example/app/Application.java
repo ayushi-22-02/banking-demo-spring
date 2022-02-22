567195f4d51d0e09fc910cf76fbdb447868a01cf
Ayushi <ayushi.khandelwal@credable.in>
2022-02-22 10:01:23 +0530

    adding beans and annotations in the project

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,25 +1,32 @@
 package org.example.app;
 
+import org.example.AppConfig;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.repository.CustomerRepositoryStub;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
 public class Application {
 
     public static void main(String[] args) {
-        CustomerRepositoryStub stubRepository =new CustomerRepositoryStub();
-        CustomerRepositoryDB dbRepository =new CustomerRepositoryDB();
 
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        CustomerService service = new DefaultCustomerService(stubRepository);
+        CustomerService service = context.getBean("customerService", CustomerService.class);
 
+//    CustomerService service = new DefaultCustomerService(new CustomerRepositoryDB());
+//    List<Customer> customers = service.getAllCustomersWithStars();
         List<Customer> customers = service.getAllCustomers();
 
         customers.forEach(System.out::println);
-
     }
+
+
+
 }
+
