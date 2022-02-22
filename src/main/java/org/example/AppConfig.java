7389a9fd9a1988908e63038bf8a938f2447032e7
Ayushi <ayushi.khandelwal@credable.in>
2022-02-22 10:46:41 +0530

    adding scope in the AppConfig

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -4,13 +4,16 @@ import org.example.repository.CustomerRepository;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean("customerService")
+    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public CustomerService getCustomerService() {
         CustomerService service = new DefaultCustomerService(getCustomerRepositoryDB());
         return service;
