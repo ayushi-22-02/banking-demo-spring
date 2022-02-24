e6a03dce7900e9c7721a0349298087175d82ec09
Ayushi <ayushi.khandelwal@credable.in>
2022-02-24 11:42:03 +0530

    fetching the data of single customer by the id

--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service("customerService")
 public class DefaultCustomerService implements CustomerService {
@@ -25,5 +26,16 @@ public class DefaultCustomerService implements CustomerService {
     public List<Customer> getAllCustomers() {
         return repository.findAll();
     }
+
+    @Override
+    public Customer getCustomer(Long id) {
+        Optional<Customer> customerbyId = repository.findById(id);
+//    if(customerbyId.isPresent())
+        return customerbyId.get();
+//    else
+//      return new Customer();
+    }
+
+
 
 }
